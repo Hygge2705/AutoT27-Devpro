99c0bbfd2713c651c3ae025deaa0edcf0838d120
Hygge2705 <thom27052003@gmail.com>
2025-06-18 21:02:19 +0700

    Day07

--- src/test/java/feature/Day06LoginTest.java
+++ src/test/java/feature/Day06LoginTest.java
@@ -1,77 +1,76 @@
 package feature;
-
-import action.LoginPage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
-import untils.Hook;
 
-public class LoginTest extends Hook {
-    LoginPage loginPage;
+public class Day06LoginTest {
+    WebDriver driver;
 
     @BeforeMethod
-    public void setupPage() {
-        loginPage = new LoginPage(driver);
+    public void setUp(){
+        driver = new EdgeDriver();
+        driver.manage().window().maximize();
     }
 
     @Test
     public void testLoginSuccessfully(){
+        driver.get("https://www.saucedemo.com/");
+
         // Kiểm tra tiêu đề và URL
         Assert.assertEquals(driver.getTitle(), "Swag Labs", "Title mismatch!");
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/", "URL mismatch!");
 
         // Điền thông tin đúng
-//        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-//        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-//        driver.findElement(By.id("login-button")).click();
-        loginPage.inputLogin("standard_user", "secret_sauce");
-        loginPage.clickLoginButton();
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
 
         // Xác nhận vào trang inventory
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html", "Login failed!");
 
         // Kiểm tra logo & sản phẩm
-//        WebElement appLogo = driver.findElement(By.className("app_logo"));
-//        Assert.assertTrue(appLogo.isDisplayed(), "Logo not displayed!");
-        loginPage.isLogoDisplayed();
+        WebElement appLogo = driver.findElement(By.className("app_logo"));
+        Assert.assertTrue(appLogo.isDisplayed(), "Logo not displayed!");
 
-//        int itemCount = driver.findElements(By.className("inventory_item")).size();
-//        Assert.assertTrue(itemCount > 0, "No products displayed!");
-        loginPage.getInventoryItemCount();
+        int itemCount = driver.findElements(By.className("inventory_item")).size();
+        Assert.assertTrue(itemCount > 0, "No products displayed!");
     }
 
     @Test
     public void testLoginWithEmptyUsername(){
+        driver.get("https://www.saucedemo.com/");
+
         // Kiểm tra tiêu đề và URL
         Assert.assertEquals(driver.getTitle(), "Swag Labs", "Title mismatch!");
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/", "URL mismatch!");
 
-        // Điền thông tin: bỏ trống username
-        loginPage.inputLogin("", "secret_sauce");
-        loginPage.clickLoginButton();
-//        driver.findElement(By.id("user-name")).sendKeys("");
-//        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-//        driver.findElement(By.id("login-button")).click();
+        // Điền thông tin đúng
+        driver.findElement(By.id("user-name")).sendKeys("");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
 
         // Xác nhận vào trang inventory
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/", "Should stay on login page!");
-//        WebElement errorMsg = driver.findElement(By.cssSelector("[data-test='error']"));
-        Assert.assertTrue(loginPage.getErrorMessage().contains("Username and password do not match") || loginPage.getErrorMessage().contains("Epic sadface"), "Unexpected error message!");
+        WebElement errorMsg = driver.findElement(By.cssSelector("[data-test='error']"));
+        Assert.assertTrue(errorMsg.isDisplayed(), "Error message not displayed!");
+        Assert.assertTrue(errorMsg.getText().contains("Username and password do not match") || errorMsg.getText().contains("Epic sadface"), "Unexpected error message!");
     }
 
     @Test
     public void testLoginWithEmptyPassword(){
+        driver.get("https://www.saucedemo.com/");
+
         // Kiểm tra tiêu đề và URL
         Assert.assertEquals(driver.getTitle(), "Swag Labs", "Title mismatch!");
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/", "URL mismatch!");
 
-        // Điền thông tin: bỏ trống password
-        loginPage.inputLogin("standard_user", "");
-        loginPage.clickLoginButton();
-//        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-//        driver.findElement(By.id("password")).sendKeys("");
-//        driver.findElement(By.id("login-button")).click();
+        // Điền thông tin đúng
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("");
+        driver.findElement(By.id("login-button")).click();
 
         // Xác nhận vào trang inventory
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/", "Should stay on login page!");
@@ -82,6 +81,8 @@ public class LoginTest extends Hook {
 
     @Test
     public void testLoginWithInvalidUsername(){
+        driver.get("https://www.saucedemo.com/");
+
         // Kiểm tra tiêu đề và URL
         Assert.assertEquals(driver.getTitle(), "Swag Labs", "Title mismatch!");
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/", "URL mismatch!");
@@ -116,6 +117,11 @@ public class LoginTest extends Hook {
         WebElement errorMsg = driver.findElement(By.cssSelector("[data-test='error']"));
         Assert.assertTrue(errorMsg.isDisplayed(), "Error message not displayed!");
         Assert.assertTrue(errorMsg.getText().contains("Username and password do not match") || errorMsg.getText().contains("Epic sadface"), "Unexpected error message!");
+    }
+
+    @AfterMethod
+    public void exit(){
+        driver.quit();
     }
 
 }
